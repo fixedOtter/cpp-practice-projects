f012e01a042bc86880b06cc1be0f92f7d97336ea
fixedOtter <fo1152rc@go.minnstate.edu>
2023-02-16 11:20:15 -0600

    pigprocessor init

---
+++ igpay-atinlay/README.md
@@ -0,0 +1,11 @@
+# igpay atinlay
+it's like english but even more confusing!
+
+## why?
+wanted to have a light challenge to bring me back into coding
+
+## for?
+people who are curious how someone else might have made a pig latin script idk
+
+## license?
+GPL v3 - check parent dir for license :)
---
+++ igpay-atinlay/pigLatin.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// made by fixedOtter
+
+/*
+  this program takes english input and gives you pig latin
+  Copyright (C) 2023  fixedOtter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+string PigProcessor(string userWords) {
+  // first creating the vowel database so i can pull the first one or multiple characters
+  vector<char> englishVowels{'a', 'e', 'i', 'o', 'u'};
+  // initializing to be an empty string so I can push to it
+  string pigOut = "";
+
+  // creating the for loop to go until there is a vowel found
+  for (int i = 0; i < userWords.length(); i++) {
+
+    // iterating through the vowel vector to check if it matches
+    for (int j = 0; j < 5; j++) {
+      
+      // if it matches then
+      if (userWords.at(i) == englishVowels.at(j)) {
+        pigOut = userWords.substr(i,) + userWords.substr(0,i) + "ay";
+      }
+
+    }
+
+  }
+
+  return pigOut;
+}
+
+/*
+  FIXME:
+  get input
+  user input validation (main? or wordhandler?)
+    make sure there's no illegal characters
+  pass input to PigP
+  process user input (in PigP or wordhandler?)
+    break down string into vector of words?
+    find word enders? (space, comma, period)
+  output PigP
+*/
+int main() {
+  cout << "thingy finishe" << endl;
+  return 0;
+}
