7610b0dd6d8d67fca73d0866a8b0f7a54cf91312
fixedOtter <fo1152rc@go.minnstate.edu>
2023-07-12 15:07:17 -0500

    updated code to work with a single word

--- igpay-atinlay/pigLatin.cpp
+++ igpay-atinlay/pigLatin.cpp
@@ -23,27 +23,45 @@ using namespace std;
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-string PigProcessor(string userWords) {
+string PigProcessor(const string userWords) {
   // first creating the vowel database so i can pull the first one or multiple characters
   vector<char> englishVowels{'a', 'e', 'i', 'o', 'u'};
   // initializing to be an empty string so I can push to it
   string pigOut = "";
+  // creating a userWords int so i don't get the unsigned int comparison annoyance
+  int wordLength = userWords.size();
 
   // creating the for loop to go until there is a vowel found
-  for (int i = 0; i < userWords.length(); i++) {
+  for (int i = 0; i < wordLength; i++) {
 
     // iterating through the vowel vector to check if it matches
     for (int j = 0; j < 5; j++) {
       
-      // if it matches then
-      if (userWords.at(i) == englishVowels.at(j)) {
-        pigOut = userWords.substr(i,1) + userWords.substr(0,i) + "ay";
+      // first if the word starts with a vowel
+      if (userWords.at(0) == englishVowels.at(j)) {
+
+        // setting output to just input word + yay
+        pigOut = userWords + "yay";
+
+        // breaking out of the loops & function so it doesn't keep running
+        return pigOut;
+
+      // otherwise going until the first vowel
+      } else if (userWords.at(i) == englishVowels.at(j)) {
+
+        // creating the length of the string without first consonant(s)
+        int firstStrLength = wordLength - i; 
+
+        // pushing back beginning characters to end
+        pigOut = userWords.substr(i,firstStrLength) + userWords.substr(0,i) + "ay";
+
+        // breaking out of the loops & function so it doesn't keep running
+        return pigOut;
       }
-
     }
-
   }
 
+  // ruh roh just incase
   return pigOut;
 }
 
@@ -61,6 +79,7 @@ string PigProcessor(string userWords) {
 */
 int main() {
   string userInput = "";
+  // would be cool if this was a normal string that was first converted with the PigProcessor
   cout << "elcomeway otay ethay igpay atinlay anslatortray" << endl;
   cout << "gib to me ur word pls" << endl;
   getline(cin, userInput);
